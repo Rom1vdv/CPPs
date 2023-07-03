173a3f69c8373f09e116233417a7395ed0d1aa38
Romain <romain.vennet@outlook.com>
2023-07-03 17:37:48 +0200

    start of the CPP : classes attributes"

--- CPP00/ex01/Contact.cpp
+++ CPP00/ex01/Contact.cpp
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 17:08:56 by romvan-d          #+#    #+#             */
-/*   Updated: 2023/07/03 17:16:38 by romvan-d         ###   ########.fr       */
+/*   Updated: 2023/07/03 17:33:03 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,3 +23,4 @@ Contact::~Contact(void)
 	std::cout << "Contact destructor has been called" << std::endl;
 	return ;
 }
+
--- CPP00/ex01/Contact.hpp
+++ CPP00/ex01/Contact.hpp
@@ -6,7 +6,7 @@
 /*   By: romvan-d <romvan-d@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/03 16:37:45 by romvan-d          #+#    #+#             */
-/*   Updated: 2023/07/03 17:12:38 by romvan-d         ###   ########.fr       */
+/*   Updated: 2023/07/03 17:32:53 by romvan-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,17 @@
 class Contact
 {
 	public:
-	
+		
 		Contact(void);
 		~Contact(void);
 
+		/* Getters */
+		std::string GetFirstName(void);
+		std::string GetLastName(void);
+		std::string GetNickName(void);
+		std::string GetPhoneNumber(void);
+		std::string GetDarkestSecret(void);
+	
 	private:
 		std::string FirstName;
 		std::string LastName;
